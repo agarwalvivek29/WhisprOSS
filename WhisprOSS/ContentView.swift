36e0d8e9778067201ca54ea25928e4d1f89d7392
Vivek Agarwal <agarwalvivek2902@gmail.com>
2026-01-14 09:02:20 +0530

    Initial Commit

---
+++ WhisprOSS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhisprOSS
+//
+//  Created by Vivek Agarwal on 14/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhisprOSS/WhisprOSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisprOSSApp.swift
+//  WhisprOSS
+//
+//  Created by Vivek Agarwal on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WhisprOSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
